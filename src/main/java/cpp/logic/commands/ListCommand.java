29d97bdc1be9b4d1acaecf12a8982e1804e55f8c
ZSYUYIIE <dr.dotiiee@gmail.com>
2026-03-15 00:30:24 +0800

    ListCommand: Edit MESSAGE_USAGE

--- src/main/java/cpp/logic/commands/ListCommand.java
+++ src/main/java/cpp/logic/commands/ListCommand.java
@@ -20,7 +20,9 @@ public abstract class ListCommand extends Command {
     public static final String MESSAGE_USAGE = ListCommand.COMMAND_WORD
             + ": Lists all items in the specified tab.\n"
             + "Parameters: <TAB>\n"
-            + "Example: " + ListCommand.COMMAND_WORD + " contacts";
+            + "Example: " + ListCommand.COMMAND_WORD + " contacts"
+            + "Example: " + ListCommand.COMMAND_WORD + " assignments"
+            + "Example: " + ListCommand.COMMAND_WORD + " classes";
 
     protected ListCommand() {
     }
