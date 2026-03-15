6ae4b5b0ff3914218e4a1df3541c39911302a9a7
Yi Zhong <tohyzhong@gmail.com>
2026-03-15 15:45:15 +0800

    Add Unallocate Assignment parser

---
+++ src/main/java/cpp/logic/parser/assignment/UnallocateAssignmentCommandParser.java
@@ -0,0 +1,51 @@
+package cpp.logic.parser.assignment;
+
+import java.util.List;
+
+import cpp.commons.core.index.Index;
+import cpp.logic.Messages;
+import cpp.logic.commands.assignment.UnallocateAssignmentCommand;
+import cpp.logic.parser.ArgumentMultimap;
+import cpp.logic.parser.ArgumentTokenizer;
+import cpp.logic.parser.CliSyntax;
+import cpp.logic.parser.Parser;
+import cpp.logic.parser.ParserUtil;
+import cpp.logic.parser.exceptions.ParseException;
+import cpp.model.assignment.AssignmentName;
+
+/**
+ * Parses input arguments and creates a new UnallocateAssignmentCommand object.
+ * Expected parameters: name and contact indices (ct/).
+ */
+public class UnallocateAssignmentCommandParser implements Parser<UnallocateAssignmentCommand> {
+
+    @Override
+    public UnallocateAssignmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                CliSyntax.PREFIX_ASSIGNMENT, CliSyntax.PREFIX_CLASS, CliSyntax.PREFIX_CONTACT);
+
+        if ((!ParserUtil.arePrefixesPresent(argMultimap, CliSyntax.PREFIX_ASSIGNMENT,
+                CliSyntax.PREFIX_CONTACT)
+                && !ParserUtil.arePrefixesPresent(argMultimap, CliSyntax.PREFIX_ASSIGNMENT,
+                        CliSyntax.PREFIX_CLASS))
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT,
+                    UnallocateAssignmentCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(CliSyntax.PREFIX_ASSIGNMENT, CliSyntax.PREFIX_CLASS,
+                CliSyntax.PREFIX_CONTACT);
+
+        AssignmentName assignmentName = ParserUtil
+                .parseAssignmentName(argMultimap.getValue(CliSyntax.PREFIX_ASSIGNMENT).get());
+
+        List<Index> contactIndices = List.of();
+        if (ParserUtil.arePrefixesPresent(argMultimap, CliSyntax.PREFIX_CONTACT)) {
+            String contactString = argMultimap.getValue(CliSyntax.PREFIX_CONTACT).orElse("");
+            contactIndices = ParserUtil.parseContactIndices(contactString);
+        }
+
+        return new UnallocateAssignmentCommand(assignmentName, contactIndices);
+    }
+
+}
